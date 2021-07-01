1e95f9fbf2aa30b9f9317a88c45c0dd945f076b9
Jakob Huber <jakob.huber@nexiles.com>
2021-07-01 09:33:04 +0200

    Add Eventbus subscriber and publisher

---
+++ bundles/org.openhab.binding.oh2mqtt/src/main/java/org/openhab/binding/oh2mqtt/internal/EventbusPublisherService.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.oh2mqtt.internal;
+
+import org.openhab.core.events.EventPublisher;
+import org.openhab.core.items.events.ItemCommandEvent;
+import org.openhab.core.items.events.ItemEventFactory;
+import org.openhab.core.library.types.OnOffType;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component(service = { EventbusPublisherService.class }, immediate = true)
+public class EventbusPublisherService {
+    private EventPublisher eventPublisher;
+
+    private final Logger logger = LoggerFactory.getLogger(EventbusPublisherService.class);
+
+    @Activate
+    protected void activate(BundleContext context) {
+        logger.info("EventBusToMQTTPublisher is activated!");
+    }
+
+    @Deactivate
+    protected void deactivate(BundleContext context) {
+        logger.info("EventBusToMQTTPublisher is deactivated!");
+    }
+
+    public void postSomething() {
+        ItemCommandEvent itemCommandEvent = ItemEventFactory.createCommandEvent("ItemX", OnOffType.ON);
+
+        String topic = itemCommandEvent.getTopic();
+        String type = itemCommandEvent.getType();
+        String payload = itemCommandEvent.getPayload();
+        logger.info(String.format("Create new event: Topic: %s, Type: %s, Payload: %s", topic, type, payload));
+
+        eventPublisher.post(itemCommandEvent);
+    }
+
+    @Reference
+    public void setEventPublisher(EventPublisher eventPublisher) {
+        this.eventPublisher = eventPublisher;
+    }
+
+    public void unsetEventPublisher(EventPublisher eventPublisher) {
+        this.eventPublisher = null;
+    }
+}
---
+++ bundles/org.openhab.binding.oh2mqtt/src/main/java/org/openhab/binding/oh2mqtt/internal/EventbusSubscriberService.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.oh2mqtt.internal;
+
+import java.util.Set;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.core.events.Event;
+import org.openhab.core.events.EventFilter;
+import org.openhab.core.events.EventSubscriber;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component(service = { EventbusSubscriberService.class, EventSubscriber.class }, immediate = true, property = {
+        "event.topics=*" })
+public class EventbusSubscriberService implements EventSubscriber {
+    private final Set<String> subscribedEventTypes = Set.of(EventSubscriber.ALL_EVENT_TYPES);
+
+    private final Logger logger = LoggerFactory.getLogger(EventbusSubscriberService.class);
+
+    @Reference
+    protected @NonNullByDefault({}) EventbusPublisherService publisher;
+
+    @Activate
+    protected void activate(BundleContext context) {
+        logger.info("EventBusToMQTTSubscriber is activated!");
+    }
+
+    @Deactivate
+    protected void deactivate(BundleContext context) {
+        logger.info("EventBusToMQTTSubscriber is deactivated!");
+    }
+
+    @Override
+    public Set<String> getSubscribedEventTypes() {
+        return subscribedEventTypes;
+    }
+
+    @Override
+    public EventFilter getEventFilter() {
+        return null;
+    }
+
+    @Override
+    public void receive(Event event) {
+        String topic = event.getTopic();
+        String type = event.getType();
+        String payload = event.getPayload();
+        String source = event.getSource();
+        logger.info(String.format("Received new event: Topic: %s, Type: %s, Payload: %s, Source: %s", topic, type,
+                payload, source));
+    }
+}
