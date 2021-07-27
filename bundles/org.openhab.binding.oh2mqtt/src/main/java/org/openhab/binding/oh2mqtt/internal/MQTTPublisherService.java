bf427d305f90c5409a81f5427d3351172debbd34
Jakob Huber <jakob.huber@nexiles.com>
2021-07-27 11:47:35 +0200

    Add auth to mqtt login

--- bundles/org.openhab.binding.oh2mqtt/src/main/java/org/openhab/binding/oh2mqtt/internal/MQTTPublisherService.java
+++ bundles/org.openhab.binding.oh2mqtt/src/main/java/org/openhab/binding/oh2mqtt/internal/MQTTPublisherService.java
@@ -1,8 +1,10 @@
 package org.openhab.binding.oh2mqtt.internal;
 
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.openhab.core.events.Event;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
@@ -11,29 +13,46 @@ import org.osgi.service.component.annotations.Deactivate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.eclipse.paho.client.mqttv3.MqttConnectOptions.MQTT_VERSION_3_1_1;
+
 @Component(service = { MQTTPublisherService.class }, immediate = true)
 public class MQTTPublisherService extends MqttClient {
     private final Logger logger = LoggerFactory.getLogger(MQTTPublisherService.class);
 
     private static final String URL = "tcp://192.168.200.93:1883";
+    private static final String MQTT_USER = "openhab";
+    private static final String MQTT_PASSWORD = "openhab";
     private static final String CLIENT_ID = "OHEB2MQTT";
     private static final String TOPIC_OUT = "oheb2mqtt/out";
     private static final String TOPIC_IN = "oheb2mqtt/in";
 
     @Activate
     public MQTTPublisherService(BundleContext context) throws MqttException {
-        super(URL, CLIENT_ID, null);
+        super(URL, CLIENT_ID, new MemoryPersistence());
 
         logger.info("MQTTPublisher is activated!");
 
-        this.connect();
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName(MQTT_USER);
+        options.setPassword(MQTT_PASSWORD.toCharArray());
 
-        this.subscribe();
+        try {
+            this.connect(options);
+            logger.debug("Successfully connected to MQTT broker: {}", URL);
+            this.subscribe();
+        } catch (MqttException e) {
+            logger.error("Cannot connect to MQTT broker", e);
+        }
     }
 
     @Deactivate
     protected void deactivate(BundleContext context) {
-        logger.info("MQTTPublisher is deactivated!");
+        try {
+            logger.debug("Disconnect from MQTT broker: {}", URL);
+            this.disconnect();
+        } catch (MqttException e) {
+            logger.error("Cannot disconnect from MQTT broker", e);
+        }
     }
 
     private void subscribe() throws MqttException {
